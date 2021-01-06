14f3e8a306cdef4103fbc949cbbea1686af72d80
mehran <mehran.hydary@gmail.com>
2021-01-06 16:56:27 -0500

    Comments added to understand code

--- src/constants/contracts.js
+++ src/constants/contracts.js
@@ -1,6 +1,10 @@
 /* eslint-disable camelcase */
 // eslint-disable-next-line import/prefer-default-export
-export const UniswapV2Router02 = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+
+// Used to approve and unapprove pool tokens
+export const UniswapV2Router02 = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'; // this is the actual uniswap router 
+// Figure out what the pool contracts are for...
+
 export const DollarPool1 = '0xdF0Ae5504A48ab9f913F8490fBef1b9333A68e68';
 export const DollarPool2 = '0xA5976897BC0081e3895013B08654DfEc50Bcb33F';
 export const DollarPool3 = '0xBBDA9B2f267b94147cB5b51653237C2F1EE69054';
--- src/constants/tokens.js
+++ src/constants/tokens.js
@@ -1,23 +1,23 @@
 export const UNI = {
-  addr: '0x88ff79eB2Bc5850F27315415da8685282C7610F9',
+  addr: '0x88ff79eB2Bc5850F27315415da8685282C7610F9', // this is a uni pair for ESD and USDC (will need to update for sBTC and ESB)
   decimals: 18,
   symbol: 'UNI',
 };
 
 export const USDC = {
-  addr: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-  decimals: 6,
-  symbol: 'USDC',
+  addr: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // this needs to be sBTC contract
+  decimals: 6, // I beleive this should be 18
+  symbol: 'USDC', // This should be updated to show sBTC
 };
 
 export const ESD = {
-  addr: '0x36F3FD68E7325a35EB768F1AedaAe9EA0689d723',
-  decimals: 18,
-  symbol: 'ESD',
+  addr: '0x36F3FD68E7325a35EB768F1AedaAe9EA0689d723', // once contract is deployed this should be updated
+  decimals: 18, // this is fine
+  symbol: 'ESD', // this will be ESB
 };
 
 export const ESDS = {
-  addr: '0x443D2f2755DB5942601fa062Cc248aAA153313D3',
-  decimals: 18,
-  symbol: 'ESDS',
+  addr: '0x443D2f2755DB5942601fa062Cc248aAA153313D3', // once contract is deployed with the proxy this will need change
+  decimals: 18, // fine
+  symbol: 'ESDS', // will become ESBS after deployed
 };
